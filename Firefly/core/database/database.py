447001e2b0b0544eec236e2f38796b14d38c47e2
Zachary Priddy <me@zpriddy.com>
2016-08-09 22:44:06 -0700

    Trying to fix DB

--- Firefly/core/database/database.py
+++ Firefly/core/database/database.py
@@ -2,19 +2,21 @@
 # @Author: Zachary Priddy
 # @Date:   2016-08-09 22:23:13
 # @Last Modified by:   Zachary Priddy
-# @Last Modified time: 2016-08-09 22:41:31
+# @Last Modified time: 2016-08-09 22:43:59
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 engine = create_engine('sqlite:///firefly.sqlite', convert_unicode=True)
-Session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+#Session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+Session = sessionmaker(bind=engine)
 db_session = Session()
 
 Base = declarative_base()
-Base.query = db_session.query_property()
+#Base.query = db_session.query_property()
 
 def init_db():
-  import core.database.models
+  from core.database.models import *
   # Make all tables
   Base.metadata.create_all(bind=engine)
