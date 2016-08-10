a5cea81ee565d4030e190426ca89ec3d6c7af650
Zachary Priddy <me@zpriddy.com>
2016-08-09 22:31:53 -0700

    Added missing import

--- Firefly/core/database/database.py
+++ Firefly/core/database/database.py
@@ -2,10 +2,10 @@
 # @Author: Zachary Priddy
 # @Date:   2016-08-09 22:23:13
 # @Last Modified by:   Zachary Priddy
-# @Last Modified time: 2016-08-09 22:27:34
+# @Last Modified time: 2016-08-09 22:31:31
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 engine = create_engine('sqlite:///firefly.sqlite', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
