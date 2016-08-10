457221e177489be2c6bb46cbb59113bad84ae427
Zachary Priddy <me@zpriddy.com>
2016-08-09 23:15:08 -0700

    Fixing except

--- Firefly/core/firefly_api.py
+++ Firefly/core/firefly_api.py
@@ -104,8 +104,8 @@ def reinstall_devices():
   try:
     deleted = DeviceDB.query.delete()
     logging.CRITICAL(str(deleted) + ' Devices Deleted')
-  except:
-    return "Error deleting devices. See log for details."
+  except ObjectDeletedError as err:
+    return "Error deleting devices. See log for details." + err.msg
 
   try:
     with open('config/devices.json') as devices:
