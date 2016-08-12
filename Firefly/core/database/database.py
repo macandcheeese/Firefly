1ac5b3e450b8d443abe1bc70b581b3a4d127e74e
Zachary Priddy <me@zpriddy.com>
2016-08-11 21:58:17 -0700

    blah

--- Firefly/core/database/database.py
+++ Firefly/core/database/database.py
@@ -2,7 +2,7 @@
 # @Author: Zachary Priddy
 # @Date:   2016-08-09 22:23:13
 # @Last Modified by:   Zachary Priddy
-# @Last Modified time: 2016-08-11 21:53:55
+# @Last Modified time: 2016-08-11 21:58:11
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -12,7 +12,7 @@ from sqlalchemy.pool import NullPool, StaticPool
 engine = create_engine('mysql://firefly:ffsql@localhost/firefly',  poolclass=NullPool)
 
 #Session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
-Session = sessionmaker(bind=engine)
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 db_session = Session()
 
 Base = declarative_base()
