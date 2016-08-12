5a67690a88f7192d61f98c5f66f93afee781e908
Zachary Priddy <me@zpriddy.com>
2016-08-11 21:21:58 -0700

    Trying something new

--- Firefly/core/database/database.py
+++ Firefly/core/database/database.py
@@ -2,22 +2,27 @@
 # @Author: Zachary Priddy
 # @Date:   2016-08-09 22:23:13
 # @Last Modified by:   Zachary Priddy
-# @Last Modified time: 2016-08-11 21:01:47
+# @Last Modified time: 2016-08-11 21:21:47
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.pool import StaticPool
 
-engine = create_engine('sqlite:///firefly.sqlite', convert_unicode=True, connect_args={'check_same_thread':False}, poolclass=StaticPool)
+#engine = create_engine('sqlite:///firefly.sqlite', convert_unicode=True, connect_args={'check_same_thread':False}, poolclass=StaticPool)
 
 #Session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
-Session = sessionmaker(bind=engine)
-db_session = Session()
+#Session = sessionmaker(bind=engine)
+#db_session = Session()
+
+engine = None
+db_session = scoped_session(lambda: create_session(bind=engine))
 
 Base = declarative_base()
 #Base.query = db_session.query_property()
 
 def init_db():
   from core.database.models import *
+  global engine
+  engine = create_engine('sqlite:///firefly.sqlite', convert_unicode=True)
   # Make all tables
   Base.metadata.create_all(bind=engine)
